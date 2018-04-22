f5c8bb409f3c68d93199c77803e0d1f3d0525889
DefinitelyNotAShark <32074072+DefinitelyNotAShark@users.noreply.github.com>
2018-04-21 19:53:54 -0500

    Added main menu and button nav for start and quit

---
+++ Assets/Scripts/ButtonScript.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ButtonScript : MonoBehaviour
+{
+    public void StartClick()
+    {
+        SceneManager.LoadScene(1);
+    }
+
+    public void CreditsClick()
+    {
+        
+    }
+
+    public void QuitClick()
+    {
+#if UNITY_EDITOR
+        // Application.Quit() does not work in the editor so
+        // UnityEditor.EditorApplication.isPlaying need to be set to false to end the game
+        UnityEditor.EditorApplication.isPlaying = false;
+#else
+         Application.Quit();
+#endif
+    }
+}
